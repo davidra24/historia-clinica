e1953b1bccb6dcaa89a2e4565aac51f9c61d4e2b
David Ramírez <cdavidramirezg24@gmail.com>
2020-08-28 00:45:44 -0500

    autorización de usuarios

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import { UserController } from '../controllers/user.controller';
+import { auth } from '../middlewares/passport';
 
 const usercontroller = new UserController();
 const api = express.Router();
 
-api.get('/users', usercontroller.getUsers);
-api.get('/users/:document', usercontroller.getOneuser);
+api.get('/users', auth, usercontroller.getUsers);
+api.get('/users/:document', auth, usercontroller.getOneuser);
 api.post('/users', usercontroller.insertUser);
-api.put('/users/:document', usercontroller.updateUser);
-api.delete('/users/:document', usercontroller.deleteUser);
+api.put('/users/:document', auth, usercontroller.updateUser);
+api.delete('/users/:document', auth, usercontroller.deleteUser);
 
 api.post('/login', usercontroller.login);
 api.post('/verify', usercontroller.validateUser);
